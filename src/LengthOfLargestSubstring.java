8a86365c157f6e2a69f2e4060708f90856ff03d7
Ben Watkins <contact@benwtks.com>
2019-10-26 22:20:44 +0100

    Initial commit

---
+++ src/LengthOfLargestSubstring.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class LengthOfLargestSubstring {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char[] sArray = scanner.nextLine().toCharArray();
+
+        int subsetStart = 0;
+        int currentSubsetSize = 0;
+        int biggestSubsetSize = 0;
+
+        for (int i = 0; i < sArray.length; i++) {
+            for (int j = subsetStart; j < i; j++) {
+                if (sArray[i] == sArray[j]) {
+                    biggestSubsetSize = Math.max(currentSubsetSize, biggestSubsetSize);
+                    currentSubsetSize -= (j - subsetStart) + 1;
+                    subsetStart = j + 1;
+                    break;
+                }
+            }
+
+            currentSubsetSize++;
+        }
+
+        biggestSubsetSize = Math.max(currentSubsetSize, biggestSubsetSize);
+
+        System.out.println(biggestSubsetSize);
+    }
+}
